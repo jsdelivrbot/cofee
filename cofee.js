e36a249c985d3637a1933932f05e1e1136568f71
Samvel <samvelgevorgyan87@gmail.com>
2017-11-26 22:55:27 +0400

    d

--- cofee.js
+++ cofee.js
@@ -1,7 +1,37 @@
-/**
- * Created by SAM on 11/26/2017.
- */
 
-$(document).ready(function () {
-    alert(0)
-})
+window.onload=function(){
+
+    var element = document.createElement("link");
+    element.setAttribute("rel", "stylesheet");
+    element.setAttribute("type", "text/css");
+    element.setAttribute("href", "https://cdn.rawgit.com/Samvel1987/cofee/master/cofee.css");
+    document.getElementsByTagName("head")[0].appendChild(element);
+
+
+    var d=document.createElement('div');
+
+    var parentElem = document.querySelector('section');
+    // document.body.appendChild(d);
+
+    insertAfter(d, parentElem);
+
+}
+
+
+function insertAfter(el, referenceNode) {
+    if(referenceNode) {
+        referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+        load_home();
+
+    }
+}
+
+
+
+function insertBefore(el, referenceNode) {
+    referenceNode.parentNode.insertBefore(el, referenceNode);
+}
+
+function load_home() {
+    document.getElementById("content").innerHTML='<object type="text/html" data="https://cdn.rawgit.com/Samvel1987/cofee/master/load.html" ></object>';
+}
