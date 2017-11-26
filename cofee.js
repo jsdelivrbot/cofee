c161708aa0735166cb0b69e7550a93c89c75a618
Samvel <samvelgevorgyan87@gmail.com>
2017-11-27 00:17:34 +0400

    d

--- cofee.js
+++ cofee.js
@@ -4,16 +4,30 @@ window.onload=function(){
     var element = document.createElement("link");
     element.setAttribute("rel", "stylesheet");
     element.setAttribute("type", "text/css");
-    element.setAttribute("href", "https://cdn.rawgit.com/Samvel1987/cofee/master/cofee.css");
+    element.setAttribute("href", "https://raw.githubusercontent.com/Samvel1987/cofee/master/cofee.css");
     document.getElementsByTagName("head")[0].appendChild(element);
 
 
-    var d=document.createElement('div');
+    var d='<div class="landing_main">'+
+        '<div>'+
+        '<h1>Get into you dream school</h1>'+
+    '</div>'+
+    '<div class="h1p1">'+
+        'Get connected to students, alumni and professional from your dream school'+
+
+    '</div>'+
+    '<div class="advisor_div">'+
+        '<button>Find Your Advisor   </button>'+
+
+    '</div>'+
+    '</div>';
+    var o = document.createElement("div");
+    o.innerHTML = d;
 
     var parentElem = document.querySelector('section');
     // document.body.appendChild(d);
 
-    insertAfter(d, parentElem);
+    insertAfter(o, parentElem);
 
 }
 
@@ -21,7 +35,6 @@ window.onload=function(){
 function insertAfter(el, referenceNode) {
     if(referenceNode) {
         referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
-        // load_home();
 
     }
 }
